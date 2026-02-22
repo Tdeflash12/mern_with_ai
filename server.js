d19935e5c545ae61368b0eea34c4b99afbb9c140
Abhesh Mandal <abheshmandal249@gmail.com>
2026-02-22 17:41:25 +0530

    node js setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const http=require("http");
+const server =http.createServer((request,response) => {
+    response.end("Hello World");
+});
+server.listen(5001,()=>{
+    console.log("Server running at port 5001")
+}); 
