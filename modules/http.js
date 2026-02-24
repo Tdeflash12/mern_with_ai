daebd0d735bd315f50d711f8cf6815436e1de87c
Abhesh Mandal <abheshmandal249@gmail.com>
2026-02-24 21:09:49 +0530

    File System

--- modules/http.js
+++ modules/http.js
@@ -1,14 +1,21 @@
 import http from 'http';
+import { url } from 'inspector';
 
 const app = http.createServer((request,response)=>{
     console.log(request.method)
-    const data = {
-        version:"01.31",
-        name:"Mern Stack",
-        staatus:"OK",
-    };
-    response.writeHead(200,{"content-type":"application/json"});
-    response.end(JSON.stringify(data))
+   console.log(request.url)
+
+   if(request.url === "/"){
+    response.writeHead(200,{ "content-type":"text/html"});
+    response.end("<h1>HOME PAGE</h1>")
+   }else if(request.url==="/about"){
+    response.writeHead(200,{"content-type":"text/html"});
+    response.end("<h1>ABOUT PAGE</h1>")
+   }else{
+     response.writeHead(404,{"content-type":"text/html"});
+    response.end("<h1>pahe not found</h1>")
+   }
+    
 });
 app.listen(5001,()=>{
     console.log("Server running at port 5001");
